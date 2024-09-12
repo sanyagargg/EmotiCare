57ddf363f030fdfefcead1adb3bb87084b130c92
sanyagargg <133576817+sanyagargg@users.noreply.github.com>
2024-09-12 17:04:21 +0530

    Initial commit

--- mongoose.js
+++ mongoose.js
@@ -1,22 +1,27 @@
-const mongoose=require('mongoose');
-mongoose.connect("mongodb+srv://rgs786999:8wm6lhVoV59yirSJ@cluster0.9olfp7w.mongodb.net/",{
-    dbName: "MHO",
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+require('dotenv').config(); // Make sure dotenv is loaded before using environment variables
+
+const mongoose = require('mongoose');
+
+mongoose.set('debug', true); // Enable debug mode
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 })
-.then(()=>{
-    console.log("connection successful");
+.then(() => {
+  console.log('Connection successful');
 })
-.catch((err)=>{
-    console.log(`No connection ${err}`);
-})
+.catch((err) => {
+  console.log(`No connection: ${err}`);
+});
 
 const logInSchema = new mongoose.Schema({
-    fname: { type: String, required: true }, // changed from Firstname
-    lname: { type: String, required: true }, // changed from Lasttname
-    email: { type: String, required: true, unique: true },
-    pass: { type: String, required: true },
-    passconfirm: { type: String, required: true },
-  });
-const LogInCollection=new mongoose.model('UserData',logInSchema);
-module.exports=LogInCollection;
+  fname: { type: String, required: true },
+  lname: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  pass: { type: String, required: true },
+  passconfirm: { type: String, required: true },
+});
+
+const LogInCollection = mongoose.model('UserData', logInSchema);
+module.exports = LogInCollection;
